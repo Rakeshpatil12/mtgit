c32e2351c99d8f86414951a647371998084a2e48
Rakeshpatil12 <rakeshpatil12496>
2019-06-14 14:45:58 +0530

    first project

---
+++ lib0fclass/lib0fclass/calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libofclass
+{
+    public class calculator
+    {
+        private int result;
+        public void add(int a, int b)
+        {
+            result = a + b;
+        }
+        public void div(int a, int b)
+        {    if (b == 0)
+                throw new DivideByZeroException();
+
+            result = a/b;
+        }
+        public int output()
+        {
+            return result;
+        }
+
+    }
+}
